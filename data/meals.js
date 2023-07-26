6ef841509ccd87715d2b4caea96d45e072744bd6
Funsho24 <oladipupofunsho99@gmail.com>
2023-07-26 15:23:31 +0100

    studied backend

---
+++ data/meals.js
@@ -0,0 +1,28 @@
+const meals = [
+  {
+    id: 1,
+    name: "Rice noodles",
+  },
+  {
+    id: 2,
+    name: "Jollof rice",
+  },
+  {
+    id: 3,
+    name: "Fried rice",
+  },
+  {
+    id: 4,
+    name: "ofada rice",
+  },
+  {
+    id: 5,
+    name: "Aduke rice",
+  },
+  {
+    id: 6,
+    name: "basmati rice",
+  },
+];
+
+module.exports = meals;
